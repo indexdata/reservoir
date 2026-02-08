95f1426a219faad73e15726f58117f3a8f86f9a8
Adam Dickmeiss <adam@indexdata.dk>
2026-02-08 16:49:11 +0100

    RSRVR-30 GitHub API fetch (#160)
    Existing raw fetches will be transformed to GitHub API content fetches.
    For example:
    https://raw.githubusercontent.com/user/repo/branch/file.js =>
    https://api.github.com/repos/user/repo/contents/file.js?ref=branch",

--- server/src/main/java/com/indexdata/reservoir/server/entity/CodeModuleEntity.java
+++ server/src/main/java/com/indexdata/reservoir/server/entity/CodeModuleEntity.java
@@ -7,10 +7,13 @@ import io.vertx.ext.web.client.WebClient;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.Tuple;
 import java.util.Objects;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.okapi.common.WebClientFactory;
 
 @SuppressWarnings({"squid:S5738","squid:S1123"})
 public class CodeModuleEntity {
+  private static final Logger log = LogManager.getLogger(CodeModuleEntity.class);
   private final String id;
   private final String type;
   private final String url;
@@ -192,18 +195,47 @@ public class CodeModuleEntity {
       return this;
     }
 
+    // we want to see if the URL is of the form https://raw.githubusercontent.com/...
+    // and if so, transform it to the corresponding api.github.com URL to avoid caching issues
+    static String transformToApiFetch(String url, String rawPrefix, String apiPrefix) {
+      if (url.startsWith(rawPrefix)) {
+        String[] parts = url.substring(rawPrefix.length()).split("/", 4);
+        if (parts.length == 4) {
+          String user = parts[0];
+          String repo = parts[1];
+          String ref = parts[2];
+          String path = parts[3];
+          return String.format("%srepos/%s/%s/contents/%s?ref=%s",
+              apiPrefix, user, repo, path, ref);
+        }
+      }
+      return url;
+    }
+
     /** Resolve url if included.
      * @param vertx vertx instance
      * @return future with built CodeModuleEntity
      */
     public Future<CodeModuleEntity> resolve(Vertx vertx) {
+      return resolve(vertx, "https://raw.githubusercontent.com/", "https://api.github.com/");
+    }
+
+    /** Resolve url if included.
+     * @param vertx vertx instance
+     * @param rawPrefix if url starts with this, it will be transformed to api fetch case
+     * @param apiPrefix prefix to use for api fetch case
+     * @return future with built CodeModuleEntity
+     */
+    Future<CodeModuleEntity> resolve(Vertx vertx, String rawPrefix, String apiPrefix) {
       String id = json.getString(ID_FIELD, "none");
       String url = json.getString(URL_FIELD);
       if (url == null || url.isEmpty()) {
         return Future.succeededFuture(build());
       }
       WebClient webClient = WebClientFactory.getWebClient(vertx);
-      return webClient.getAbs(url)
+      final String url2 = transformToApiFetch(url, rawPrefix, apiPrefix);
+      log.info("Fetching module '{}' from URL {}", id, url2);
+      return webClient.getAbs(url2)
         .send()
         .map(response -> {
           if (response.statusCode() < 200 || response.statusCode() >= 300) {
@@ -211,7 +243,28 @@ public class CodeModuleEntity {
                 String.format("Config error: cannot retrieve module '%s' at %s (%d)",
                     id, url, response.statusCode()));
           }
-          json.put(SCRIPT_FIELD, response.bodyAsString());
+          if (url2.startsWith(apiPrefix)) {
+            JsonObject bodyObject = response.bodyAsJsonObject();
+            String sha = bodyObject.getString("sha");
+            if (sha == null) {
+              throw new RuntimeException(
+                  String.format("Config error: cannot retrieve module '%s' at %s: "
+                  + "no sha field in API response", id, url2));
+            }
+            log.info("Module {} SHA {}", id, sha);
+            String content = bodyObject.getString("content");
+            if (content == null) {
+              throw new RuntimeException(
+                  String.format("Config error: cannot retrieve module '%s' at %s: "
+                    + "no content field in API response", id, url2));
+            }
+            // content is base64 encoded, we need to decode it
+            byte[] decodedBytes = java.util.Base64.getMimeDecoder().decode(content);
+            String decodedContent = new String(decodedBytes);
+            json.put(SCRIPT_FIELD, decodedContent);
+          } else {
+            json.put(SCRIPT_FIELD, response.bodyAsString());
+          }
           return build();
         });
     }
@@ -267,7 +320,5 @@ public class CodeModuleEntity {
 
 
   }
-
-
 
 }
