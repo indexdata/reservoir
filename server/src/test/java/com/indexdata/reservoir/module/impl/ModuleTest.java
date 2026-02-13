3019c22a4c3e889f085f231271c14184e66f4218
Adam Dickmeiss <adam@indexdata.dk>
2026-02-13 13:08:01 +0100

    Unused import

--- server/src/test/java/com/indexdata/reservoir/module/impl/ModuleTest.java
+++ server/src/test/java/com/indexdata/reservoir/module/impl/ModuleTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
-import com.hazelcast.internal.json.Json;
 import com.indexdata.reservoir.module.ModuleCache;
 import com.indexdata.reservoir.module.ModuleExecutable;
 import com.indexdata.reservoir.module.ModuleInvocation;
